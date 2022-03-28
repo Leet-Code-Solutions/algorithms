980fff5d944bb0d27555ba87b3b791610d45f873
Shaul <39487412+shaul-xu@users.noreply.github.com>
2022-03-28 21:59:28 +0800

    add comment

--- 0704/BinarySearch.ts
+++ 0704/BinarySearch.ts
@@ -3,17 +3,17 @@ function search(nums: number[], target: number): number {
   let end = nums.length - 1;
 
   while (start <= end) {
-    const mid = start + ~~((end - start) / 2);
+    const mid = start + ~~((end - start) / 2); // get mid pointer
     const cur = nums[mid];
-    if (cur === target) {
+    if (cur === target) { // if mid value equal to target return the mid
       return mid;
     }
-    if (cur < target) {
+    if (cur < target) { // if mid value is less than target should move the left pointer to mid+1
       start = mid + 1;
-    } else {
+    } else { // else if mid value is greater than target should move the right pointer to mid-1
       end = mid - 1;
     }
   }
 
-  return -1;
+  return -1; // if can't find target return -1
 }
