445c6c99511e080f6592bd8e12a3a3a10ff17c2e
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2022-03-28 18:47:20 +0800

    35

---
+++ 35/Meodinger.c
@@ -0,0 +1,23 @@
+int biSearch(int target, int *nums, int l, int r)
+{
+    int m, v;
+    while (l <= r)
+    {
+        m = l + (r - l) / 2;
+        v = nums[m];
+
+        if (v == target)
+            return m;
+
+        if (v < target)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return l;
+}
+
+int searchInsert(int *nums, int numsSize, int target)
+{
+    return biSearch(target, nums, 0, numsSize - 1);
+}
